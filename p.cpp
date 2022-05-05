b6d6473a4d75fe8b0a8c4556f18e7a0687a1eaf3
minhsoaicavip5515 <minhsoaicavip5515@gmail.com>
2022-05-05 16:16:55 +0700

    sua file c++

--- p.cpp
+++ p.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <bits/stdc++.h>
 
+void duma(int &x)
+{
+	x++;
+	std::cout << x << std::endl;
+}
+
 int main()
 {
-	std::string x[5] = {"aa", "cc", "ff", "rr", "qq"};
-	std::string *p1 = x;
-	std::cout << sizeof(x) << "---" << sizeof(p1)<< "\n";
+	int f = 55;
+	duma(f);
 	return 0;
 }
